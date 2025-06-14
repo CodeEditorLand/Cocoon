e8f34152ef9605cf9eeb401f7d769dd683bd4940
Nikola Hristov <Nikola@PlayForm.Cloud>
2025-06-14 21:36:01 +0300

    Delete stale files

--- Source/Core/RequireInterceptor/Factory.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @module Factory (RequireInterceptor)
- * @description Aggregates and exports all module factories used by the
- * `RequireInterceptor` service. This serves as a single point of import
- * for the different factory implementations.
- */
-
-export type { Interface as INodeModuleFactory } from "./Factory/Interface.js";
-export { VSCodeNodeModuleFactory } from "./Factory/VSCode.js";
--- Source/PatchProcess/ProcessPatch.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @module ProcessPatch
- * @description This module defines the service that provides the necessary native
- * functions and configuration for the other process patching Effects. It serves
- * as the public entry point for the ProcessPatch service.
- */
-
-export { Live } from "./ProcessPatch/Live.js";
-export { Tag, type Interface } from "./ProcessPatch/Service.js";
