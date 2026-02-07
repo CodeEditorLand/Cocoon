c332193e7bd767968bff05cf5c710c3fcbac7fb9
Nikola Hristov <Nikola@PlayForm.Cloud>
2026-02-07 19:57:50 +0200

    Define ITerminalService interface

---
+++ Source/Interfaces/ITerminalService.ts
@@ -0,0 +1,16 @@
+/**
+ * @module Interfaces/ITerminalService
+ * @description
+ * Interface definition for Terminal Service.
+ */
+
+import { Context } from "effect";
+
+export interface ITerminalService {
+    createTerminal(name: string, shellPath?: string, cwd?: string): Promise<number>;
+    sendText(terminalId: number, text: string): Promise<void>;
+    resize(terminalId: number, cols: number, rows: number): Promise<void>;
+    kill(terminalId: number): Promise<void>;
+}
+
+export const ITerminalService = Context.Tag<ITerminalService>();
