f754402bf7ff1703e44ede5f08a804cb063b2674
Nikola Hristov <Nikola@PlayForm.Cloud>
2026-04-11 17:04:04 +0300

    feat(Cocoon): Add IFileSystemService interface export
    Add a new interface file at Source/Interfaces/IFileSystemService.ts that re-exports the IFileSystemService from the Services module. This provides a clean interface module location for file system operations.
    
    The Target/ directory contains the compiled JavaScript output from this TypeScript interface file.

---
+++ Source/Interfaces/IFileSystemService.ts
@@ -0,0 +1,1 @@
+export { IFileSystemService } from "../Services/FileSystemService.js";
---
+++ Target/Interfaces/IFileSystemService.js
@@ -0,0 +1,1 @@
+import{IFileSystemService as i}from"../Services/FileSystemService.js";export{i as IFileSystemService};
